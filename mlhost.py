7c64988837392c940a584e0f4df6a80744cf3ae1
aronf <aronf@DESKTOP-3Q639PG>
2023-05-20 12:48:55 +0200

    f

--- mlhost.py
+++ mlhost.py
@@ -1,14 +1,14 @@
 import streamlit as st
 from PIL import Image
-import cv2
+#import cv2
 from tensorflow.keras.models import load_model
 import tensorflow as tf
 import numpy as np
 
 def preprocess_image(image):
-    image = cv2.resize(image, (224, 224))  # Resize the image to the input size expected by MobileNet
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # Convert BGR to RGB
-    image = tf.expand_dims(image, axis=0)  # Add a batch dimension
+    #image = cv2.resize(image, (224, 224))  # Resize the image to the input size expected by MobileNet
+    #image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # Convert BGR to RGB
+    #image = tf.expand_dims(image, axis=0)  # Add a batch dimension
     return image
 
 def make_prediction(image, model):
@@ -37,11 +37,12 @@ def main():
 
     if uploaded_file is not None:
         # Display the uploaded image
-        image = cv2.imdecode(np.fromstring(uploaded_file.read(), np.uint8), 1)
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image = Image.open(uploaded_file)
+        #image = cv2.imdecode(np.fromstring(uploaded_file.read(), np.uint8), 1)
+        #image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         st.image(image, caption="Uploaded Image", use_column_width=True)
         image = np.array(image)
-        image = cv2.resize(image, (224, 224))
+        #image = cv2.resize(image, (224, 224))
         
 
         print(image)
